b43a31510c35ce0051a2a4168211d1358a29d562
gluay88 <p.wantagaew@gmail.comm>
2022-05-15 08:43:39 -0700

    fetch user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  // data state // storage
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadData();
+    // getData();
+  }, []);
+
+  const loadData = async () => {
+    await fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((receivedData) => setData(receivedData));
+  };
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>User Branch / Fetch API</h3>
+      {data.map(user => (
+        <div key={user.id}>
+          <p>Name: {user.name}</p>
+          <p>Phone: {user.phone}</p>
+          <p>User Name: {user.username}</p>
+          <p>website: {user.website}</p>
+          <br/>    
+        </div>
+      ))}
     </div>
   );
 }
