a7d28d60aba539aee33abb84c62929f95fb8ca45
gluay88 <p.wantagaew@gmail.comm>
2022-05-16 12:29:13 -0700

    photo api added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import {Images} from "./components/Images";
 
 function App() {
+  const [images, setImages] = useState([]);
+
+  const fetchAPI = async () => {
+    const response = await axios.get(
+      "https://api.unsplash.com/photos/?client_id=7DEpDeiDKdZRli0RhIerGd5qJLyKaDb7TE_h0v7BsQg"
+    );
+    console.log(response.data);
+    const data = await response.data;
+    setImages(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <br></br>
+      <button className="btn btn-primary btn-md" onClick={fetchAPI}>
+        Get Photo from API
+      </button>
+      <div className="photos">
+        {images.length > 0 && <Images images={images} />}
+      </div>
+      <br></br>
+      <p style={{textAlign:'center'}}>Copyright &copy; 2022 | Gluay S. Wang</p>
     </div>
   );
 }
