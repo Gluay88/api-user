bf6e19d38b6a4d31b5767bca3d07283880b8261a
gluay88 <p.wantagaew@gmail.comm>
2022-05-16 12:53:32 -0700

    api-unsplash added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import {Images} from "./components/Images";
 
 function App() {
   const [images, setImages] = useState([]);
+  const API_URL = 'https://api.unsplash.com/photos/?client_id=dCMltc8sOE92zKag0-mWW36616HW87DDZMiD-mHRNRg'
 
   const fetchAPI = async () => {
-    const response = await axios.get(
-      "https://api.unsplash.com/photos/?client_id=dCMltc8sOE92zKag0-mWW36616HW87DDZMiD-mHRNRg"
-    );
+    const response = await axios.get(API_URL);
     console.log(response.data);
     const data = await response.data;
     setImages(data);
@@ -19,11 +18,12 @@ function App() {
       <button className="btn btn-primary btn-md" onClick={fetchAPI}>
         Get Photo from API
       </button>
+      <p style={{marginTop:'1rem'}}>Gluay S. Wang</p>
       <div className="photos">
         {images.length > 0 && <Images images={images} />}
       </div>
       <br></br>
-      <p style={{textAlign:'center'}}>Copyright &copy; 2022 | Gluay S. Wang</p>
+      
     </div>
   );
 }
